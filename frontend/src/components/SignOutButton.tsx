cc896c505a3b6b7e4fcee8215f3cb8070f0366d5
unknown <patrickjohndonado@gmail.com>
2024-03-09 15:40:40 +0800

    added auth & auth tests & initial layout

---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+
+const SignOutButton = () => {
+  const queryClient = useQueryClient();
+  const { showToast } = useAppContext();
+  const mutation = useMutation(apiClient.signOut, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries("validateToken");
+      showToast({ message: "Signed Out!", type: "SUCCESS" });
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const handleClick = () => {
+    mutation.mutate();
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="text-secondary px-3 rounded-full font-bold bg-white hover:bg-gray-100"
+    >
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOutButton;
