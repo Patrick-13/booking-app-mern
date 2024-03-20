8a6c8b901315dac79159ca7704f693597a6922b0
unknown <patrickjohndonado@gmail.com>
2024-03-20 21:34:26 +0800

    added hotel detail page and e2e test

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -47,24 +47,28 @@ const SearchBar = () => {
       </div>
 
       <div className="flex bg-white px-2 py-1 gap-2">
-        <label className="items-center flex">Adults: </label>
-        <input
-          className="w-full p-1 focus:outline-none font-bold"
-          type="number"
-          min={1}
-          max={20}
-          value={adultCount}
-          onChange={(e) => setAdultCount(parseInt(e.target.value))}
-        />
-        <label className="items-center flex">Children: </label>
-        <input
-          className="w-full p-1 focus:outline-none font-bold"
-          type="number"
-          min={0}
-          max={20}
-          value={childCount}
-          onChange={(e) => setChildCount(parseInt(e.target.value))}
-        />
+        <label className="items-center flex">
+          Adults:
+          <input
+            className="w-full p-1 focus:outline-none font-bold"
+            type="number"
+            min={1}
+            max={20}
+            value={adultCount}
+            onChange={(e) => setAdultCount(parseInt(e.target.value))}
+          />
+        </label>
+        <label className="items-center flex">
+          Children:
+          <input
+            className="w-full p-1 focus:outline-none font-bold"
+            type="number"
+            min={0}
+            max={20}
+            value={childCount}
+            onChange={(e) => setChildCount(parseInt(e.target.value))}
+          />
+        </label>
       </div>
       <div>
         <DatePicker
