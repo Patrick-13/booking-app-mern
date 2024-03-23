0604689c20b2b978b6a2f5929679ca4e6ae246db
unknown <patrickjohndonado@gmail.com>
2024-03-23 17:14:45 +0800

    added payment method and e2e payment test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,8 @@ import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Details";
+import Booking from "./pages/Booking";
+import Home from "./pages/Home";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -23,7 +25,7 @@ function App() {
           path="/"
           element={
             <Layout>
-              <p>Home Page</p>
+              <Home />
             </Layout>
           }
         />
@@ -62,6 +64,14 @@ function App() {
 
         {isLoggedIn && (
           <>
+            <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
+            />
             <Route
               path="/add-hotel"
               element={
